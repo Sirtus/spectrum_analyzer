6eba62db51545cb714dedc17d1de398f9d5858d2
Sirtus <tslonina@student.agh.edu.pl>
2022-04-06 15:42:52 +0200

    added trigonometric package body

--- trigonometric.vhd
+++ trigonometric.vhd
@@ -1,1 +1,12 @@
- 
+package trigonometric is
+
+    function apprrox_sin(x: integer) return integer;;
+    
+end package;
+
+package body trigonometric is
+    function apprrox_sin(x: integer) return integer is
+        begin
+            
+        end function
+end package body;
