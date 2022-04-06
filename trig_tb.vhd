8bd62b91f8ecc94518ebb63da07c5e75e16a8c2b
Sirtus <tslonina@student.agh.edu.pl>
2022-04-06 17:16:47 +0200

    added cos function

--- trig_tb.vhd
+++ trig_tb.vhd
@@ -14,13 +14,19 @@ architecture sim of trig_tb is
 begin
 
     process is
-        variable v: integer:= 0;
+        variable v_sin, v_cos: integer:= 0;
     begin
         for i in  0 to 32 loop
-            v := app_sin(i);
-            report "sin(" & integer'image(i) & ") = " & integer'image(v);
+            v_sin := app_sin(i);
+            report "sin(" & integer'image(i) & ") = " & integer'image(v_sin);
         end loop;
+        for i in  0 to 32 loop
+            v_cos := app_cos(i);
+            report "cos(" & integer'image(i) & ") = " & integer'image(v_cos);
+        end loop;
+        
         wait;
+        
     end process;
 
 
